ae076347b4646999759680aba2d33fc410f979e2
karma <karmatashi954@gmail.com>
2022-01-11 22:06:18 +0545

    add of calculator buttons with enum and switches

--- calculatorApp/ContentView.swift
+++ calculatorApp/ContentView.swift
@@ -6,13 +6,76 @@
 //
 
 import SwiftUI
+enum CalculatorButtons: String {
+    case zero, one, two, three, four, five, six, seven, eight, nine, dot
+    case equals, plus, minus, multiply, divide
+    case ac, plusMinus, percent
+    
+    var title: String {
+        switch self {
+        case .dot:
+            return "."
+        case .zero:
+            return "0"
+        case .one:
+            return "1"
+        case .two:
+            return "2"
+        case .three:
+            return "3"
+        case .four:
+            return "4"
+        case .five:
+            return "5"
+        case .six:
+            return "6"
+        case .seven:
+            return "7"
+        case .eight:
+            return "8"
+        case .nine:
+            return "9"
+        case .equals:
+            return "="
+        case .plus:
+            return "+"
+        case .minus:
+            return "-"
+        case .multiply:
+            return  "x"
+        case .divide:
+            return "÷"
+        case .ac:
+            return "AC"
+        case .plusMinus:
+            return "±"
+        case .percent:
+            return "%"
+        default:
+            return "AC"
+        }
+    }
+    
+    var backgroundColor : Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .dot:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+        default :
+            return Color(.orange)
+        
+        }
+    }
+}
 
 struct ContentView: View {
-    let buttons = [
-        ["7","8","9","X"],
-        ["4","5","6","-"],
-        ["1","2","3","+"],
-        ["0",".",".","="],
+    let buttons:[[CalculatorButtons]] = [
+        [.ac,.plusMinus,.percent,.divide],
+        [.seven,.eight,.nine,.multiply],
+        [.four,.five,.six,.minus],
+        [.one,.two,.three,.plus],
+        [.zero,.dot,.equals],
     ]
     var body: some View {
         ZStack(alignment: .bottom){
@@ -27,14 +90,23 @@ struct ContentView: View {
                 
                     
                 ForEach(buttons, id:  \.self){row in
+                    
                     HStack (spacing: 12){
                         ForEach(row, id: \.self){button in
-                            Text(button)
-                                .font(.system(size: 32))
-                                .foregroundColor(Color.white)
-                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
-                                .background(Color.gray)
-                                .cornerRadius(50)
+                            // all the buttons are iterating so also encapsulate inside a button
+                            Button(action: {
+                                
+                            })
+                            // the view of the button
+                            {
+                                    Text(button.title)
+                                        .font(.system(size: 32))
+                                        .foregroundColor(Color.white)
+                                        .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                                        .background(button.backgroundColor)
+                                        .cornerRadius(self.buttonWidth(button: button))
+                            }
+                            
                         }
                 }
                 
@@ -53,7 +125,10 @@ struct ContentView: View {
         }
         
     }
-    func buttonWidth()-> CGFloat{
+    func buttonWidth(button: CalculatorButtons)-> CGFloat{
+        if button == .zero{
+            return (UIScreen.main.bounds.width - 4 * 12) / 4*2
+        }
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
