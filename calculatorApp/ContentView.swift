ff56a41ac56728efb8c49fa3820b3481ecf52100
karma <karmatashi954@gmail.com>
2022-01-11 21:03:43 +0545

    add of the UI of the calculator

--- calculatorApp/ContentView.swift
+++ calculatorApp/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    let buttons = [
+        ["7","8","9","X"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".",".","="],
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .bottom){
+            Color.black
+                .ignoresSafeArea()
+            VStack ( spacing: 12){
+                HStack{
+                    Spacer()
+                    Text("42").foregroundColor(Color.white).font(.system(size: 64))
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                    
+                ForEach(buttons, id:  \.self){row in
+                    HStack (spacing: 12){
+                        ForEach(row, id: \.self){button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .foregroundColor(Color.white)
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .background(Color.gray)
+                                .cornerRadius(50)
+                        }
+                }
+                
+    //                Text("8")
+    //                    .font(.system(size: 32))
+    //                Text("8")
+    //                    .font(.system(size: 32))
+    //                Text("8")
+    //                    .font(.system(size: 32))
+    //                Text("8")
+    //                    .font(.system(size: 32))
+                    
+                }
+            }
+            .padding(.all)
+        }
+        
+    }
+    func buttonWidth()-> CGFloat{
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
