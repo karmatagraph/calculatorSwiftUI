d44439d411740e8793c0898a4187b4bdd2596e4e
karma <karmatashi954@gmail.com>
2022-01-11 20:13:16 +0545

    Initial Commit

---
+++ calculatorApp/calculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorAppApp.swift
+//  calculatorApp
+//
+//  Created by karma on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
